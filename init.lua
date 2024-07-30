afaf7db9538545e5d23cd168cce1b5a6bc84b252
jstrot <44594069+jstrot@users.noreply.github.com>
2024-07-29 20:02:37 -0400

    Add explicit dependency of nvim-lspconfig on cmp-nvim-lsp (#1042)

--- init.lua
+++ init.lua
@@ -438,6 +438,9 @@ require('lazy').setup({
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Allows extra capabilities provided by nvim-cmp
+      'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
       local nvim_lsp = require('lspconfig');
