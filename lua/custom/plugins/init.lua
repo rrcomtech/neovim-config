40391a86aba7c11a9bcd2d727fcd4d1b3853d14f
Robert Richter <robert.richter@rrcomtech.com>
2024-08-02 21:08:09 +0200

    Adding git blame plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -84,5 +84,15 @@ return {
   },
   {
     'github/copilot.vim'
+  },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+        enabled = true,
+        message_template = "<author> • <summary> • <date>",
+        date_format = "%r",
+        virtual_text_column = 10
+    }
   }
 }
